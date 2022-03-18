15af5972d7aee747215369573b9889ce74a06f3b
kai <kai@kaiconsulting.co>
2022-03-18 07:45:04 -0700

    wired up blog controller

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -5,21 +5,43 @@ const {
 
 module.exports = {
     // create blog
+    createBlog: async (req, res) => {
+        const { title, content } = req.body;
+        try {
+            const blog = await Blog.create({
+                title,
+                content,
+            });
+            res.json(blog);
+            console.log(blog);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+
     // get all blogs
+    getAllBlogs: async (req, res) => {
+        try {
+            const blogData = await Blog.findAll({});
+            const blogs = blogData.map(blog => blog.get({ plain: true }));
+            res.render('allBlogs', {
+                blogs,
+            });
+        } catch (e) {
+            res.json(e);
+        }
+    },
+
     // single blog
-
-    // findBlog: (req, res) => {
-    //     // TODO - req.query.id
-
-    //     const blogId = req.query.id || req.params.id;
-
-    //     Blog.findOne({
-    //         where: {
-    //             id: blogId,
-    //         },
-    //         include: Comment
-    //     })
-    // }
+    getBlogById: async (req, res) => {
+        try {
+            const blogData = await Blog.findByPk(req.params.blogId);
+            const blog = blogData.get({ plain: true });
+            res.render('singleBlog', {
+                blog
+            });
+        } catch (e) {
+            res.json(e);
+        }
+    },
 };
-
-// /blog/${id}
