18f2198508b590b0a848c61955bbfbb2d5e39177
kai <kai@kaiconsulting.co>
2022-05-04 13:06:50 -0600

    added js files to main.handlebars

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const { loginView, signupView } = require('../controllers/userController');
 const { getUserBlogs, getAllBlogs, renderBlog } = require('../controllers/blogController');
 
 router.get('/', loginView);
-router.get('/homepage', getAllBlogs);
-router.get('/dashboard', getUserBlogs);
+router.get('/homepage', getUserBlogs);
+router.get('/dashboard', getAllBlogs);
 router.get('/newblog', renderBlog);
 router.get('/login', loginView);
 router.get('/signup', signupView);
